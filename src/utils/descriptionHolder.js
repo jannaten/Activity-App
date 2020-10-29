96adb12b0bb2a068ed2b3a3775652cb2bf329ae1
Jannaten Nayem <jannaten.nayem@eng.tamk.fi>
2020-10-29 09:30:26 +0200

    Lot of changes on suggestions text

--- src/utils/descriptionHolder.js
+++ src/utils/descriptionHolder.js
@@ -3,12 +3,10 @@ import React from "react";
 function descriptionHolder({ desc }) {
   if (desc === "clear sky")
     return (
-      <>
-        <ul>
-          <li>Clear Sky outside</li>
-          <li>Go out and do your activity</li>
-        </ul>
-      </>
+      <ul>
+        <li>Clear Sky outside</li>
+        <li>Go out and do your activity</li>
+      </ul>
     );
   else if (desc === "few clouds")
     return (
@@ -190,154 +188,154 @@ function descriptionHolder({ desc }) {
   else if (desc === "light rain")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its raning lightly.</li>
+        <li>Take umbrella while doing activities</li>
       </ul>
     );
   else if (desc === "moderate rain")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its now raining moderately outside</li>
+        <li>Take umbrella while doing activities</li>
       </ul>
     );
   else if (desc === "heavy intensity rain")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its raining with heavy intensity</li>
+        <li>No need to go outside</li>
       </ul>
     );
   else if (desc === "very heavy rain")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its very heavyly raining outside</li>
+        <li>No need to go outside</li>
       </ul>
     );
   else if (desc === "extreme rain")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its extreamly raining outside</li>
+        <li>No need to go outside</li>
       </ul>
     );
   else if (desc === "freezing rain")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its freezing outside and alos raining</li>
+        <li>Take a good jacket and umbrella while outside</li>
       </ul>
     );
   else if (desc === "light intensity shower rain")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its showely rain with light intensity</li>
+        <li>Make sure you've your umbrella with you</li>
       </ul>
     );
   else if (desc === "shower rain")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its showerly raining outside.</li>
+        <li>Make sure you've your umbrella with you</li>
       </ul>
     );
   else if (desc === "heavy intensity shower rain")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>There's shower rain with heavy intensity</li>
+        <li>Dont go out for activites</li>
       </ul>
     );
   else if (desc === "ragged shower rain")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its raggy showery and rainy outside</li>
+        <li>Take umbrealla while going out</li>
       </ul>
     );
   else if (desc === "light snow")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its {desc} outside.</li>
+        <li>You can go and do activities</li>
       </ul>
     );
   else if (desc === "Snow")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Theres good snow outside.</li>
+        <li>You can go and play with the snows</li>
       </ul>
     );
   else if (desc === "Heavy snow")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its heavy snow outside.</li>
+        <li>You can go and play with the snows</li>
       </ul>
     );
   else if (desc === "Sleet")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its quite sleety outside</li>
+        <li>Careful while doing activties outside</li>
       </ul>
     );
   else if (desc === "Light shower sleet")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>There is light crystal ice outside</li>
+        <li>No need to use winter shoes so far</li>
       </ul>
     );
   else if (desc === "Shower sleet")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Ices are now a crustal outside.</li>
+        <li>Make sure you have your windter shoes</li>
       </ul>
     );
   else if (desc === "Light rain and snow")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its raining lightly with some snow</li>
+        <li>You can go out to do some activites</li>
       </ul>
     );
   else if (desc === "Rain and snow")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its raining and slowing same time</li>
+        <li>Take a jacket and umbrella while outside</li>
       </ul>
     );
   else if (desc === "Light shower snow")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its snowing very lightly outside.</li>
+        <li>You surely can do snow activities</li>
       </ul>
     );
   else if (desc === "Shower snow")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its {desc} outside.</li>
+        <li>You surely can do snow activities</li>
       </ul>
     );
   else if (desc === "Heavy shower snow")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Heavy shower snow is now outside</li>
+        <li>Not appropiate to do activities outside</li>
       </ul>
     );
   else if (desc === "Smoke")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
+        <li>Its smoky outside.</li>
         <li>Try to use mask while walking</li>
       </ul>
     );
@@ -351,78 +349,78 @@ function descriptionHolder({ desc }) {
   else if (desc === "sand/ dust whirls")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
+        <li>Its quite sandy outside.</li>
         <li>Try to use mask while walking</li>
       </ul>
     );
   else if (desc === "fog")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its quite foggy outside</li>
+        <li>Be casefull outside while walking</li>
       </ul>
     );
   else if (desc === "sand")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its really sandy outside</li>
+        <li>Protect you nose, mouth, eyes while outside</li>
       </ul>
     );
   else if (desc === "dust")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its really dusty outside</li>
+        <li>Better not to go outside</li>
       </ul>
     );
   else if (desc === "volcanic ash")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Volcanic ash are coming out</li>
+        <li>Don't do activities near there</li>
       </ul>
     );
   else if (desc === "squalls")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Sudden violent wind and storm may happend</li>
+        <li>No need to go outside</li>
       </ul>
     );
   else if (desc === "tornado")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Tornonado is striking hard</li>
+        <li>Go some safe place as soon as possible</li>
       </ul>
     );
   else if (desc === "few clouds: 11-25%")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Looks there is few clouds outside</li>
+        <li>You can start your activities</li>
       </ul>
     );
   else if (desc === "scattered clouds: 25-50%")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Clouds are pretty scattered outside</li>
+        <li>Rain may happend within an hour.</li>
       </ul>
     );
   else if (desc === "broken clouds: 51-84%")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its almost half overcast outside</li>
+        <li>Make sure to take an umbrella</li>
       </ul>
     );
   else if (desc === "overcast clouds: 85-100%")
     return (
       <ul>
-        <li>Its hazzy outside.</li>
-        <li>Try to use mask while walking</li>
+        <li>Its almost overcast outside</li>
+        <li>Make sure to take an umbrella</li>
       </ul>
     );
   else return <p>The weather is like now {desc}</p>;
