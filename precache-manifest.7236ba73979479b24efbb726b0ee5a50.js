0e4d3c689f4ac7bb45c18e42f28671928d2bff59
Jannaten Nayem <jannaten.nayem@eng.tamk.fi>
2020-11-01 22:05:21 +0200

    Updates

---
+++ precache-manifest.7236ba73979479b24efbb726b0ee5a50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456ce902462fa11da1f5329abb235834",
+    "url": "/Activity-App/index.html"
+  },
+  {
+    "revision": "60d6edc259115ee99b46",
+    "url": "/Activity-App/static/css/main.eb244c84.chunk.css"
+  },
+  {
+    "revision": "46599644f862db2c2a08",
+    "url": "/Activity-App/static/js/2.a7206920.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/Activity-App/static/js/2.a7206920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d6edc259115ee99b46",
+    "url": "/Activity-App/static/js/main.2419ac66.chunk.js"
+  },
+  {
+    "revision": "eb94641ae2330a1b9570",
+    "url": "/Activity-App/static/js/runtime-main.375469d0.js"
+  }
+]);
