1d3c59b8660e90959e2679e51b5ca3204f34beb2
Jannaten Nayem <jannaten.nayem@eng.tamk.fi>
2020-11-02 00:19:37 +0200

    Redux logger console removed

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,13 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 
-const middleware = [logger];
+// const middleware = [logger];
+let middleware = [];
+if (process.env.NODE_ENV === "development") {
+  middleware = [...middleware, logger];
+} else {
+  middleware = [...middleware];
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middleware));
 
